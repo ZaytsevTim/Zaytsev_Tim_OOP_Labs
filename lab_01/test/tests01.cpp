344c60acabbd991f8b1912172cdf886457064fe1
ZaytsevTim <159259376+ZaytsevTim@users.noreply.github.com>
2025-09-21 01:11:52 +0300

    Update tests01.cpp

--- lab_01/test/tests01.cpp
+++ lab_01/test/tests01.cpp
@@ -1,20 +1,32 @@
 #include <gtest/gtest.h>
 #include "../include/num_to_32bit.h"
 
-TEST(test_01, basic_test_set)
+TEST(test_01, test_set)
 {
     ASSERT_TRUE(num_to_32bit(1)=="00000000000000000000000000000001");
 }
 
-TEST(test_02, basic_test_set)
+TEST(test_02, test_set)
 {
     ASSERT_TRUE(num_to_32bit(275)=="00000000000000000000000100010011");
 }
 
-TEST(test_03, basic_test_set)
+TEST(test_03, test_set)
 {
     ASSERT_TRUE(num_to_32bit(1073741825)=="01000000000000000000000000000001");
 }
+
+TEST(test_04, test_set)
+{
+    ASSERT_TRUE(num_to_32bit(0)=="00000000000000000000000000000000");
+}
+
+TEST(test_05, test_set)
+{
+    ASSERT_TRUE(num_to_32bit(2147483647)=="01111111111111111111111111111111");
+}
+
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
